81dbe744ea61774817683bbeb146df1c1da0850e
David <davidmunyiri2@gmail.com>
2020-08-30 05:58:12 +0300

    created empty templates(main.html, store.html, check_out.html) and views(store,cart,check_out)

--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def store(request):
+    context ={}
+    return render(request,'store/store.html',context)
+
+def cart(request):
+    context ={}
+    return render(request,'store/cart.html',context)
+
+def check_out(request):
+    context ={}
+    return render(request,'store/check_out.html',context)
