787f2bc3eda508466df78987f12760e5aaebc1b1
David <davidmunyiri2@gmail.com>
2020-08-31 19:21:01 +0300

    product images showing in front-end

--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 # Create your models here.
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
@@ -16,7 +17,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
-    # image
+    product_image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
