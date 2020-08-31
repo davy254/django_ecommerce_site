aa84b86c5c1e4b1d324457fba5c0df67a78abd7e
David <davidmunyiri2@gmail.com>
2020-08-31 15:09:04 +0300

    Finished designing cart.html, store.html, check_out.html

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = {
+urlpatterns = [
     path('', views.store, name='store'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.check_out, name='check-out'),
-}
+]
+
