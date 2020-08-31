e1dcc3a54732d50b2b93b9d82fc2167b78654e65
David <davidmunyiri2@gmail.com>
2020-08-31 22:01:09 +0300

    Modified Product model -- if no image is provided the a default image will be shown

--- store/models.py
+++ store/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
-    product_image = models.ImageField(null=True, blank=True)
+    product_image = models.ImageField(default='placeholder.png',null=True, blank=True)
 
     def __str__(self):
         return self.name
