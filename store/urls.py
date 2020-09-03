3fc6b242b9545e9a442579f040728adb2500e61c
David <davidmunyiri2@gmail.com>
2020-09-03 18:19:28 +0300

    finished cart functionality

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.store, name='store'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.check_out, name='check-out'),
+    path('update-item/', views.update_item, name='update-item'),
+    path('process-order/', views.processOrder, name='process-order'),
 ]
+
 
